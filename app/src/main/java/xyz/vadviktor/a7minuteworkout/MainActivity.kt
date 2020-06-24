97d5764bfad93c5bf5cb9c4cb0eb6c1df98b0098
Viktor (Icon) VAD <vad.viktor+github@gmail.com>
2020-06-24 19:34:04 +0100

    init

---
+++ app/src/main/java/xyz/vadviktor/a7minuteworkout/MainActivity.kt
@@ -0,0 +1,19 @@
+package xyz.vadviktor.a7minuteworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun startExercise(view: View) {
+        Toast.makeText(this, "Here we will start", Toast.LENGTH_SHORT).show()
+    }
+}
